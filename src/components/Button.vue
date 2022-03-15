fef8a6b7db596fc99a7ea2a2b5001fac3638863d
Devjit Choudhury <devjitc1@gmail.com>
2022-03-15 20:59:39 +0530

    made header and button component

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+    <button 
+        class="btn" 
+        :style="{background: color}"
+        @click="onCLicking()"
+    >{{ text }}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+        color: String,
+    },
+    methods: {
+        onCLicking() {
+            console.log("Clicked");
+        }
+    }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button 
+            text="Add Task" 
+            color="blue" 
+        />
+        <!-- <Button text="Done Task" color="green" />
+        <Button text="Delete Task" color="red" /> -->
+    </header>
+</template>
+
+<script>
+import Button from '@/components/Button';
+export default {
+    name: ' Header',
+    props: {
+        title: {
+            type: String,
+            default : 'Hello World',
+        }
+    },
+    components: {
+        Button
+    },
+}
+</script>
+
+<style scoped>
+    header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+    }
+</style>
